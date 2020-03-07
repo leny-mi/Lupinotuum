e6a2c83daadf524a8cd7c1665d674be0222cce1e
Inluminous <lenmisch@gmail.com>
2020-03-07 19:49:57 +0100

    Add main file

---
+++ lupinotuum.py
@@ -0,0 +1,19 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'test':
+            #await message.channel.send('pong')
+            print("Received")
+
+client = MyClient()
+file = open("token.txt","r")
+token = file.readline()
+client.run(token)
